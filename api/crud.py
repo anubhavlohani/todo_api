8afd6c2c0a5db91c8b9a81827be4385b8ce9d681
Anubhav Lohani <anubhavlohani4@gmail.com>
2023-02-22 17:24:33 +0530

    added func to update item

--- api/crud.py
+++ api/crud.py
@@ -1,4 +1,4 @@
-import datetime
+import uuid
 from typing import Union
 
 from fastapi import HTTPException
@@ -24,8 +24,10 @@ def create_user(db: Database, user: schemas.User) -> bool:
 # return all todo-items for given user
 def user_items(db: Database, username: str) -> list[dict]:
     collection = db['items']
-    query_result = collection.find({'username': username}, {'_id': 0})
+    query_result = collection.find({'username': username})
     curr_items = [item for item in query_result]
+    for item in curr_items:
+        item['_id'] = str(item['_id'])
     return curr_items
 
 # find a specific todo item from a specific user
@@ -38,6 +40,14 @@ def find_item(db: Database, username: str, title: str) -> dict:
 def create_item(db: Database, user: schemas.User, item_data: schemas.TodoItem) -> bool:
     collection = db['items']
     item_data = item_data.dict()
+    item_data['_id'] = str(uuid.uuid4())
     item_data['username'] = user.username
     insertion_successful = collection.insert_one(item_data)
     return True if insertion_successful else False
+
+# update an item
+def update_item(db: Database, item_id: str, new_item_data: dict) -> bool:
+    collection = db['items']
+    update_query = {"$set": new_item_data}
+    update_successful = collection.update_one({'_id': item_id}, update_query)
+    return True if update_successful else False
